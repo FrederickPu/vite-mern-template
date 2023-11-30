94dec6667ffafc414b44ae25a2cbff9b07a3adf5
FrederickPu <pufrederick51@gmail.com>
2023-11-30 09:21:02 -0500

    added get example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function App() {
-  return <p>Hello World</p>;
+  const [data, setData] = React.useState("")
+
+  function handleClick(){
+    fetch("http://localhost:8000/setData", {
+      method: 'POST', 
+      mode: 'cors',
+      body: JSON.stringify({lol : "sheckles"})
+    })
+  }
+
+  React.useEffect(() => {
+    fetch("http://localhost:8000/getData")
+      .then(response => {
+        return response.json()
+      }).then(res => setData(JSON.stringify(res)))
+  }, [])
+  return (
+    <>
+    <p>Your Data: {data}</p>
+    <button onClick={handleClick}>Set stuff</button>
+    </>
+  )
 }
 
 export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,34 @@
-console.log("Hello World");
+import { appendFileSync } from "fs";
+
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/getData', (req, res) => {
+    console.log('lol')
+  res.send({bruh : 'Successful response.'});
+});
+
+app.get('/setData', function(req, res){
+    // Access the data from the request body
+    const data = req.body;
+
+    // Perform any processing or data storage logic here
+    console.log('Received data:', data);
+
+    // Respond to the client
+    res.send({ message: 'Data received successfully.' });
+    // fs.writeFile('./data.json', {stuff : req.body}, 'utf8', err => {
+    //     if (err) {
+    //       console.log(`Error writing file: ${err}`)
+    //     } else {
+    //       console.log(`File is written successfully!`)
+    //     }
+    //   })
+})
+
+app.listen(8000, () => console.log('Example app is listening on port 8000.'));
